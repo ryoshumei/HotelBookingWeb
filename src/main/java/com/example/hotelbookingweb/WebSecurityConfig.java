3dde534765c43ff4206d195f065bb5e227e01cbf
daydreamer <s419922572@gmail.com>
2023-03-25 15:40:39 +0900

    rollback security file,updated security setting, added index's background img

--- src/main/java/com/example/hotelbookingweb/WebSecurityConfig.java
+++ src/main/java/com/example/hotelbookingweb/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.hotelbookingweb;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -32,6 +33,10 @@ public class WebSecurityConfig {
 
 
         ).authorizeHttpRequests(authz -> authz
+                .requestMatchers("/css/**")
+                .permitAll()
+                .requestMatchers("/img/**")
+                .permitAll()
                 .requestMatchers("/")
                 .permitAll()
                 .requestMatchers("/roomsInformation")
