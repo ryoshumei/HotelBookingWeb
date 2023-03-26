d153f3dd438fdf6fa7c2d67d0a7ac1ceb2420d73
daydreamer <s419922572@gmail.com>
2023-03-26 13:35:37 +0900

    updated security setting for static javascript

--- src/main/java/com/example/hotelbookingweb/WebSecurityConfig.java
+++ src/main/java/com/example/hotelbookingweb/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.hotelbookingweb;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -36,6 +35,8 @@ public class WebSecurityConfig {
                 .requestMatchers("/css/**")
                 .permitAll()
                 .requestMatchers("/img/**")
+                .permitAll()
+                .requestMatchers("/js/**")
                 .permitAll()
                 .requestMatchers("/")
                 .permitAll()
