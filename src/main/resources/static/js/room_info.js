d150cf1de9c53c2df3b231cfa8b0f3e58edbc095
daydreamer <s419922572@gmail.com>
2023-03-26 19:44:15 +0900

    updated frontend

---
+++ src/main/resources/static/js/room_info.js
@@ -0,0 +1,46 @@
+const roomInfoBtns = document.querySelectorAll('[id="check_detail"]');
+const roomInfoBtnsLength = roomInfoBtns.length
+const cards = document.querySelectorAll(".card")
+const cardsLength = cards.length
+const roomInfoBtnInitText = roomInfoBtns[0].textContent
+let selected = false;
+
+//cards[0].classList.toggle("active")
+for(let i = 0; i < roomInfoBtnsLength; i++){
+
+    roomInfoBtns[i].addEventListener("click", function (){
+        console.log("BTn be clicked")
+        cards[i].classList.toggle("active")
+
+        if(roomInfoBtns[i].textContent === roomInfoBtnInitText){
+            roomInfoBtns[i].textContent = "Back"
+            selected = true
+        } else {
+            roomInfoBtns[i].textContent = roomInfoBtnInitText
+            selected = false
+        }
+
+        if(selected){
+            hideAllOthers(i)
+        }else {
+            releaseAll()
+        }
+
+    })
+}
+
+function hideAllOthers(index){
+    for(let i = 0; i < cardsLength; i++){
+        if(i != index){
+            cards[i].style.display="none";
+        }
+    }
+}
+
+function releaseAll(){
+    for(let i = 0; i < cardsLength; i++){
+        cards[i].style.display="block";
+    }
+}
+
+
