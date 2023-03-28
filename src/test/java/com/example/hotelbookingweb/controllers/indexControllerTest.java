e70ec58d8688c7a6d175a103cbd4eac0ffe143aa
daydreamer <s419922572@gmail.com>
2023-03-28 15:06:12 +0900

    Added test file

---
+++ src/test/java/com/example/hotelbookingweb/controllers/indexControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.hotelbookingweb.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class indexControllerTest {
+
+    @Test
+    void index() {
+
+    }
+
+    @Test
+    void searchRooms() {
+    }
+}
