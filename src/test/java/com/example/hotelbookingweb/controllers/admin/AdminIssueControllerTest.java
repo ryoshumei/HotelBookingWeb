c9df5e5a24af941b376357f1bc17a4c7bfbbf010
daydreamer <s419922572@gmail.com>
2023-04-02 20:38:39 +0900

    Finished MockMVCTest

---
+++ src/test/java/com/example/hotelbookingweb/controllers/admin/AdminIssueControllerTest.java
@@ -0,0 +1,75 @@
+package com.example.hotelbookingweb.controllers.admin;
+
+import com.example.hotelbookingweb.WebSecurityConfig;
+import com.example.hotelbookingweb.entities.GuestEntity;
+import com.example.hotelbookingweb.services.GuestsService;
+import com.example.hotelbookingweb.services.RoomsService;
+import com.example.hotelbookingweb.services.admin.AdminService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.hotelbookingweb.controllers.admin.AdminIssueController.CHECK_GUESTS_PATH;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AdminIssueController.class)
+class AdminIssueControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    GuestsService guestsService;
+    @MockBean
+    RoomsService roomsService;
+    @MockBean
+    AdminService adminService;
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void checkGuests() throws Exception {
+        GuestEntity guestEntity = new GuestEntity(1,"test",204,26,0, LocalDate.of(2023,3,28),LocalDate.of(2023,4,2));
+        List<GuestEntity> guestEntityList = new ArrayList<>();
+        guestEntityList.add(guestEntity);
+
+        given(guestsService.findAllGuests()).willReturn(guestEntityList);
+
+        mockMvc.perform(get(CHECK_GUESTS_PATH))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/checkGuests"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void checkRooms() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(AdminIssueController.CHECK_ROOMS_PATH))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("admin/checkRooms"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("roomsList"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void deleteGuest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(AdminIssueController.DELETE_GUEST_PATH, 1)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/admin/checkGuests"));
+    }
+}
--- src/test/java/com/example/hotelbookingweb/controllers/users/RoomInfoControllerTest.java
+++ src/test/java/com/example/hotelbookingweb/controllers/users/RoomInfoControllerTest.java
@@ -1,21 +1,64 @@
 package com.example.hotelbookingweb.controllers.users;
 
 import com.example.hotelbookingweb.WebSecurityConfig;
+import com.example.hotelbookingweb.input_form.BookingForm;
+import com.example.hotelbookingweb.input_form.InputDateForm;
+import com.example.hotelbookingweb.services.BookingService;
+import com.example.hotelbookingweb.services.GuestsService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+
+import static com.example.hotelbookingweb.controllers.users.RoomInfoController.CONFIRM_BOOKING_PATH;
+import static com.example.hotelbookingweb.controllers.users.RoomInfoController.ROOMS_INFORMATION_PATH;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(RoomInfoController.class)
 @Import(WebSecurityConfig.class)
 class RoomInfoControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    BookingService bookingService;
+
+    @MockBean
+    GuestsService guestsService;
+
     @Test
-    void confirmBooking() {
+    void confirmBooking() throws Exception {
+
+        BookingForm bookingForm = new BookingForm("test", 202, 26,0 ,LocalDate.of(2023,3,28),LocalDate.of(2023,4,2));
+
+        mockMvc.perform(post(CONFIRM_BOOKING_PATH)
+                        .flashAttr("bookingForm",bookingForm)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("users/bookingComplete"));
     }
 
     @Test
-    void showRooms() {
+    void showRooms() throws Exception {
+
+        InputDateForm inputDateForm = new InputDateForm(LocalDate.of(2023,3,28),LocalDate.of(2023,4,2),2);
+
+        String checkInDate = inputDateForm.getCheckInDate().toString();
+
+        mockMvc.perform(get(ROOMS_INFORMATION_PATH)
+                        .param("checkInDate",checkInDate)
+                        .param("checkOutDate",inputDateForm.getCheckOutDate().toString())
+                        .param("roomNum","402")
+                        .param("numOfPeople","2"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("users/guestBookingForm"));
     }
 }
