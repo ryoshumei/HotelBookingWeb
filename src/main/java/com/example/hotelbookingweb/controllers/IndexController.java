86ac3f5cb757c70c752ad76e99c9620c8c19aba2
daydreamer <s419922572@gmail.com>
2023-08-04 18:09:08 +0900

    deleted some useful lines

--- src/main/java/com/example/hotelbookingweb/controllers/IndexController.java
+++ src/main/java/com/example/hotelbookingweb/controllers/IndexController.java
@@ -1,25 +1,13 @@
 package com.example.hotelbookingweb.controllers;
 
-
-import com.example.hotelbookingweb.entities.OrderEntity;
-import com.example.hotelbookingweb.entities.RoomEntity;
 import com.example.hotelbookingweb.input_form.InputDateForm;
-import com.example.hotelbookingweb.services.GuestsServiceImpl;
-import com.example.hotelbookingweb.services.OrdersService;
-import com.example.hotelbookingweb.services.RoomsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
